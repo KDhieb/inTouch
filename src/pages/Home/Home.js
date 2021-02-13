dd51e1f18cfb4deeed8a4487160977a083031bdd
jzql <jackylin@MacBook-Pro.local>
2021-02-12 21:54:31 -0800

    google sign in

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import classes from './Home.module.css';
+import firebase from "firebase";
 
 export default function Home() {
   return (
     <div>
-      This is home
+      <button 
+         onClick={() => { 
+          const googleAuth = new firebase.auth.GoogleAuthProvider(); 
+          firebase.auth().signInWithPopup(googleAuth);
+          firebase.auth()
+            .getRedirectResult()
+            .then((result) => {
+              if (result.credential) {
+                /** @type {firebase.auth.OAuthCredential} */
+                var credential = result.credential;
+                var token = credential.accessToken;
+              }
+              
+              var user = result.user;
+            }).catch((error) => {
+              console.log(error)
+            });
+          }} > 
+          Sign in with Google 
+        </button>
     </div>    
   )
 }
