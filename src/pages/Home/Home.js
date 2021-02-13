4ee818e6fa167a88692d729ab97bc1f42951f216
gabriellahartanto <gabriella.hartanto@icloud.com>
2021-02-13 12:48:27 +0700

    sign in and sign up functionality

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect, useContext, useState } from "react";
 import classes from './Home.module.css';
+import { UserContext } from "../../providers/UserProvider";
+import { useHistory } from 'react-router-dom';
+import { logOut } from "../../services/firebase";
 
 export default function Home() {
+  const user = useContext(UserContext);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (!user) {
+      history.push('/sign-up');
+    }
+  }, [user]);
+
   return (
     <div>
       This is home
+      <button onClick={logOut}>logout</button>
     </div>    
   )
 }
