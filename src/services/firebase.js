60b0a1b93b887e6814441da9e2a544f6bde93492
finnthedawg <42992404+finnthedawg@users.noreply.github.com>
2021-02-14 02:19:58 +0400

    Merge

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -9,17 +9,25 @@ firebase.initializeApp(config.firebaseConfig);
 
 export const auth = firebase.auth();
 const googleAuth = new firebase.auth.GoogleAuthProvider()
+googleAuth.addScope("https://www.googleapis.com/auth/calendar");
+googleAuth.addScope("https://www.googleapis.com/auth/calendar.events");
+console.log("Calendar")
 export const signInWithGoogle = () => {
-  auth.signInWithPopup(googleAuth);
-  auth.getRedirectResult()
+  auth.signInWithPopup(googleAuth)
   .then((result) => {
     if (result.credential) {
       /** @type {firebase.auth.OAuthCredential} */
       var credential = result.credential;
       var token = credential.accessToken;
+      console.log("Credentials are", credential)
+      console.log("Access token", credential.accessToken)
+      localStorage.setItem("token", token);
     }
     var user = result.user;
-    console.log(user);
+    console.log("Refresh token", user.refreshToken);
+    console.log("UID", user.uid)
+    localStorage.setItem("UID", user.uid);
+    console.log(user)
   }).catch((error) => {
     console.log(error.message)
   })
