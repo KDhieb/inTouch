8af31104268efac2430ff0ed467064602f21c6c8
KDhieb <khaliddhieb@gmail.com>
2021-02-14 03:37:06 -0500

    Cleaned up calendar.js

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -4,7 +4,7 @@ import firebase, { database } from "../../services/firebase";
 import "whatwg-fetch";
 import Switch from "devextreme-react/switch";
 import Scheduler, { Resource, View } from "devextreme-react/scheduler";
-import { data, dataObject, slotType, eventType } from "./data.js";
+import { slotType, eventType } from "./data.js";
 import CustomStore from "devextreme/data/custom_store";
 import * as AspNetData from "devextreme-aspnet-data-nojquery";
 import { UserContext } from "../../providers/UserProvider";
@@ -27,138 +27,58 @@ function Calendar() {
       snapshot.forEach(function (childSnapshot) {
         var key = childSnapshot.key;
         var childData = childSnapshot.val();
-        // console.log(childData);
         let data = {
           startDate: childData.startDate,
           endDate: childData.endDate,
-          // text: "Install New Router in Dev Room",
-          // priorityId: 1,
-          // startDate: new Date("2021-05-16T20:00:00.000Z"),
-          // endDate: new Date("2021-05-16T22:30:00.000Z"),
           text: childData.text,
           description: childData.description,
-          // eventTypeId: 3,
+          eventTypeId: childData.eventTypeId,
           // sourceTypeId: 2,
         };
         newSource.push(data);
       });
       console.log(newSource);
       setNewSource(newSource);
-      // return newSource;
     });
-    // setNewSource(newSource)
-    // return newSource;
   }
 
   useEffect(() => {
     getFreeSlotData();
   }, []);
 
-  // function getData() {}
-  // let newSource = getFreeSlotData();
   console.log("outside");
 
   console.log(newSource);
-
-  // let data = {
-  //   text: "Install New Router in Dev Room",
-  //   priorityId: 1,
-  //   startDate: new Date("2021-05-16T20:00:00.000Z"),
-  //   endDate: new Date("2021-05-16T22:30:00.000Z"),
-  // };
-
-  // let newSource = [];
-  // newSource.push(data);
-
-  // const dataObject = [];
-
-  // const currentDate = new Date();
-
-  // const getFreeSlotData = () => {
-  //   var query = firebase.database().ref(`${userId}/freeSlots`).orderByKey();
-
-  //   query.once("value").then(function (snapshot) {
-  //     snapshot.forEach(function (childSnapshot) {
-  //       var key = childSnapshot.key;
-  //       var childData = childSnapshot.val();
-  //       let data = {
-  //         startDate: childData.startDate,
-  //         endDate: childData.endDate,
-  //         text: childData.text,
-  //         description: childData.description,
-  //         eventTypeId: 3,
-  //         sourceTypeId: 2,
-  //       };
-  //       dataObject.push(data);
-  //     });
-  //   });
-  // };
-
-  // getFreeSlotData();
-
-  // const dataSource = AspNetData.createStore({
-  //   key: "userId",
-  //   // loadUrl: `https://intouch-3f91c-default-rtdb.firebaseio.com/${userId}.json`,
-  //   // insertUrl: `${url}/Post`,
-  //   // updateUrl: `${url}/Put`,
-  //   // deleteUrl: `${url}/Delete`,
-  //   onBeforeSend(_, ajaxOptions) {
-  //     ajaxOptions = {
-  //       Authorization: process.env.REACT_APP_API_KEY,
-  //     };
-  //     ajaxOptions.xhrFields = { withCredentials: true };
-  //   },
-  // });
-
-  // var request = require("request");
-  // var options = {
-  //   method: "GET",
-  //   url: `https://intouch-3f91c-default-rtdb.firebaseio.com/${userId}.json`,
-  //   headers: {
-  //     auth: process.env.REACT_APP_API_KEY,
-  //   },
-  // };
-  // request(options, function (error, response) {
-  //   if (error) throw new Error(error);
-  //   console.log(response.body);
-  // });
 
   const currentDate = new Date(2021, 4, 21);
 
   console.log(currentDate);
   const views = ["week"];
 
-  // const groups = ["priorityId"];
-
   const addNewSlot = (timeslot) => {
-    // firstTimeUser();
     var ts = timeslot.appointmentData;
     var text = ts.text;
     var description = ts.description;
     var startDate = ts.startDate;
     var endDate = ts.endDate;
+    var eventTypeId = ts.eventTypeId;
+    if (!eventTypeId) {
+      eventTypeId = 3;
+    }
 
     var newData = {
       startDate: startDate.toString(),
       endDate: endDate.toString(),
       text: text,
       description: description,
+      eventTypeId: eventTypeId,
     };
+
     console.log(`${text} ${description} ${startDate} ${endDate}`);
     const newSlotRef = firebase.database().ref(`${userId}/freeSlots`);
     newSlotRef.push(newData);
 
-    var testData = {
-      startDate: startDate.toString(),
-      endDate: endDate.toString(),
-      text: text,
-      description: description,
-      eventTypeId: 3,
-      sourceTypeId: 2,
-    };
-
-    data.push(newData);
-    console.log(dataObject);
+    console.log(timeslot);
   };
 
   const firstTimeUser = () => {
@@ -256,22 +176,6 @@ function Calendar() {
     });
   };
 
-  // constructor() {
-  // super();
-  // const user = useContext(UserContext);
-
-  // this.state = {
-  //   groupByDate: true,
-  // };
-  // this.onGroupByDateChanged = this.onGroupByDateChanged.bind(this);
-  // }
-  // onGroupByDateChanged(args) {
-  //   this.setState({
-  //     groupByDate: args.value,
-  //   });
-  // }
-
-  // render() {
   return (
     <div id="scheduler">
       <Scheduler
@@ -288,8 +192,6 @@ function Calendar() {
         onAppointmentAdded={addNewSlot}
         onAppointmentDeleted={onDelete}
         onAppointmentUpdating={update}
-        // onAppointmentUpdated={updateTest}
-        // onCurrentDateChange={firstTimeUser}
       >
         <Resource
           fieldExpr="sourceTypeId"
@@ -314,7 +216,7 @@ function Calendar() {
         />
       </Scheduler>
       <div className="options">
-        <div className="caption">Show Friend Openings </div>
+        {/* <div className="caption">Show Friend Openings </div> */}
         <div className="option">
           {/* <Switch
             value={this.state.groupByDate}
