34e6173facf6069976ba91ec75225e9728042969
gabriellahartanto <gabriella.hartanto@icloud.com>
2021-02-14 08:04:24 +0700

    fixed index list of pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,6 @@
 export { default as Home } from './Home/Home';
+export { default as Profile } from './Profile/Profile';
+export { default as Interests } from './Interests/Interests';
 export { default as Dashboard } from './Dashboard/Dashboard';
 export { default as SignIn } from './SignIn/SignIn';
 export { default as Friends } from './Friends/Friends';
