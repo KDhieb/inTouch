09be4f6a8f7be6dce7a6ca8a6e4d24af45cb8ce2
KDhieb <khaliddhieb@gmail.com>
2021-02-14 07:11:34 -0500

    comitting algo work

--- src/pages/Friends/Friends.js
+++ src/pages/Friends/Friends.js
@@ -1,125 +1,144 @@
-import React, { useState, useEffect, useContext } from 'react';
-import classes from './Friends.module.css';
-import Navbar from '../../components/Navbar/Navbar';
+import React, { useState, useEffect, useContext } from "react";
+import classes from "./Friends.module.css";
+import Navbar from "../../components/Navbar/Navbar";
 import { UserContext } from "../../providers/UserProvider";
 import { database } from "../../services/firebase";
+import { getTimes, overlap } from "../../components/Calendar/algo";
 
 export default function Friends() {
   const user = useContext(UserContext);
-  const [friend, setFriend] = useState('');
+  const [friend, setFriend] = useState("");
   const [canAdd, setCanAdd] = useState(false);
-  const [friendUid, setFriendUid] = useState('');
+  const [friendUid, setFriendUid] = useState("");
   const [friendsUid, setFriendsUid] = useState([]);
   const [friends, setFriends] = useState([]);
 
+  console.log(overlap(getTimes(["U2h82SJHjas91ASA", "K92jnsA8scjchaus8shf"])));
+
   useEffect(() => {
-    database.ref(user.uid).child('friends').on('value', (user) => {
-      const data = user.val();
-      if (data) {
-        console.log('17, DATA', data);
-        let uidList = [];
-        for (let friend of Object.keys(data)) {
-          uidList.push(data[friend]);
+    database
+      .ref(user.uid)
+      .child("friends")
+      .on("value", (user) => {
+        const data = user.val();
+        if (data) {
+          console.log("17, DATA", data);
+          let uidList = [];
+          for (let friend of Object.keys(data)) {
+            uidList.push(data[friend]);
+          }
+          setFriendsUid(uidList);
         }
-        setFriendsUid(uidList);
-      }
-    })
+      });
   }, []);
 
   useEffect(() => {
     if (friendsUid) {
-      database.ref('/').get()
-      .then((snapshot) => {
-        const data = snapshot.val();
-        let friendsList = [];
-        friendsUid.map((uid) => {
-          for (let user of Object.keys(data)) {
-            if (user === uid) {
-              friendsList.push({
-                uid,
-                name: data[user].name,
-                email: data[user].email
-              });
+      database
+        .ref("/")
+        .get()
+        .then((snapshot) => {
+          const data = snapshot.val();
+          let friendsList = [];
+          friendsUid.map((uid) => {
+            for (let user of Object.keys(data)) {
+              if (user === uid) {
+                friendsList.push({
+                  uid,
+                  name: data[user].name,
+                  email: data[user].email,
+                });
+              }
             }
-          }
+          });
+          setFriends(friendsList);
         });
-        setFriends(friendsList);
-      })
     }
   }, [friendsUid]);
 
   const onFriendChange = (event) => {
     setFriend(event.target.value);
     console.log(event.target.value);
-  }
+  };
 
   const addFriend = () => {
-    database.ref('/').get()
-    .then((snapshot) => {
-      const data = snapshot.val();
-      console.log('DB', data);
-      for (let user of Object.keys(data)) {
-        if (data[user].email === friend) {
-          console.log('USERRR', user, friend, data[user]);
-          console.log('works!');
-          setFriendUid(user);
-          setCanAdd(true);
+    database
+      .ref("/")
+      .get()
+      .then((snapshot) => {
+        const data = snapshot.val();
+        console.log("DB", data);
+        for (let user of Object.keys(data)) {
+          if (data[user].email === friend) {
+            console.log("USERRR", user, friend, data[user]);
+            console.log("works!");
+            setFriendUid(user);
+            setCanAdd(true);
+          }
         }
-      }
-    })
-    .catch(err => {
-      console.log(err);
-    });
-  }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   useEffect(() => {
     if (canAdd && friendUid.length > 0) {
       setCanAdd(false);
       const uid = friendUid;
-      setFriend('');
-      setFriendUid('');
-      console.log('can add!')
-      database.ref(`/${user.uid}`).child('friends').push(uid);
+      setFriend("");
+      setFriendUid("");
+      console.log("can add!");
+      database.ref(`/${user.uid}`).child("friends").push(uid);
     }
   }, [canAdd, friendUid]);
 
   return (
     <div>
-      <Navbar/>
-      <div className={ classes.container }>
-        <div className={ classes.left }>
-          <div className={ classes.leftTop }>
-            <h2 className={ classes.title }>Your Friends</h2>
+      <Navbar />
+      <div className={classes.container}>
+        <div className={classes.left}>
+          <div className={classes.leftTop}>
+            <h2 className={classes.title}>Your Friends</h2>
             {/* <h2 className={ classes.plus }>+</h2> */}
           </div>
-          <div className={ classes.addFriendContainer }>
+          <div className={classes.addFriendContainer}>
             <input
-              className={ classes.input }
+              className={classes.input}
               name="friend"
               placeholder="Enter an email"
               type="email"
-              value={ friend }
-              onChange={ onFriendChange }
+              value={friend}
+              onChange={onFriendChange}
             />
-            <button onClick={ addFriend } className={ classes.addBtn }>Add Friend</button>
+            <button onClick={addFriend} className={classes.addBtn}>
+              Add Friend
+            </button>
+
+            <div></div>
           </div>
           {/* <p>{ JSON.stringify(friendsUid) }</p> */}
           {/* <p>{ JSON.stringify(friends) }</p> */}
-          {
-            friends.map((friend, index) => {
-              return (
-                <div className={ classes.friendContainer } styles={ index === 0 || index === friends.length - 1 ? { border: 'none' } : { borderBottom: '2px black', borderColor: 'black' }}>
-                  <h4 className={ classes.friendName }>{ friend.name }</h4>
-                  <p className={ classes.friendEmail }>{ friend.email }</p>
-                </div>
-              )
-            })
-          }
+          {friends.map((friend, index) => {
+            return (
+              <div
+                className={classes.friendContainer}
+                styles={
+                  index === 0 || index === friends.length - 1
+                    ? { border: "none" }
+                    : { borderBottom: "2px black", borderColor: "black" }
+                }
+              >
+                <h4 className={classes.friendName}>{friend.name}</h4>
+                <p className={classes.friendEmail}>{friend.email}</p>
+              </div>
+            );
+          })}
         </div>
-        <div className={ classes.right }>
-          {/* for the recommendations */}
-        </div>
+        <div className={classes.right}>{/* for the recommendations */}</div>
       </div>
-    </div>    
-  )
+
+      <p>Testing</p>
+    </div>
+  );
 }
