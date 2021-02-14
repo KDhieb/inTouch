fe48b8b24787c203ab27ad419028b466ccd37478
KDhieb <khaliddhieb@gmail.com>
2021-02-13 20:06:11 -0500

    Fixed add and delete from db. Began update func.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import React from "react";
-import firebase from "./services/firebase";
+import firebase, { database } from "./services/firebase";
 
 import "whatwg-fetch";
 import Switch from "devextreme-react/switch";
@@ -8,6 +8,7 @@ import Scheduler, { Resource, View } from "devextreme-react/scheduler";
 import { data, priorityData } from "./data.js";
 import CustomStore from "devextreme/data/custom_store";
 
+const dataObject = {};
 // const currentDate = new Date();
 
 const currentDate = new Date(2021, 4, 21);
@@ -17,12 +18,16 @@ const views = ["week"];
 
 const groups = ["priorityId"];
 
-const onAdded = (timeslot) => {
+const userId = "userabc123";
+
+const addNewSlot = (timeslot) => {
+  // firstTimeUser();
   var ts = timeslot.appointmentData;
   var title = ts.text;
   var description = ts.description;
   var startDate = ts.startDate;
   var endDate = ts.endDate;
+
   var data = {
     startDate: startDate.toString(),
     endDate: endDate.toString(),
@@ -30,21 +35,96 @@ const onAdded = (timeslot) => {
     description: description,
   };
   console.log(`${title} ${description} ${startDate} ${endDate}`);
-  const newSlotRef = firebase.database().ref("db/user1232");
+  const newSlotRef = firebase.database().ref(`${userId}/freeSlots`);
   newSlotRef.push(data);
+  // const childRef = firebase.database().ref(`${userId}/freeSlots`);
+};
+
+const firstTimeUser = () => {
+  var newUserObj = {
+    interests: [],
+    freeSlots: [],
+  };
+
+  const newUserRef = firebase.database().ref(userId);
+  newUserRef.push(newUserObj);
 };
 
 const onDelete = (timeslot) => {
-  console.log("delete!");
+  var query = firebase.database().ref(`${userId}/freeSlots`).orderByKey();
+  query.once("value").then(function (snapshot) {
+    snapshot.forEach(function (childSnapshot) {
+      var key = childSnapshot.key;
+      var childData = childSnapshot.val();
+      var ts = timeslot.appointmentData;
+      if (
+        ts.startDate === childData.startDate &&
+        ts.endDate === childData.endDate
+      ) {
+        firebase.database().ref(`${userId}/freeSlots`).child(key).remove();
+        return true; // break out of loop
+      }
+    });
+  });
 };
 
-const onUpdate = (timeslot) => {
-  console.log("Update!");
+// let oldStartDateForUpdate;
+// let oldendDateForUpdate;
+
+// const storeUnchangedSlot = (object) => {
+//   var ts = object.oldData;
+//   oldStartDateForUpdate = ts.startDate;
+//   oldendDateForUpdate = ts.endDate;
+// };
+
+const updateTest = (object) => {
+  console.log(object);
+};
+
+const update = (object) => {
+  var ts = object.oldData;
+  let oldStartDateForUpdate = ts.startDate;
+  let oldendDateForUpdate = ts.endDate;
+
+  var title = object.text;
+  var description = object.description;
+  var startDate = object.startDate;
+  var endDate = object.endDate;
+
+  var data = {
+    startDate: startDate.toString(),
+    endDate: endDate.toString(),
+    title: title,
+    description: description,
+  };
+
+  var query = firebase.database().ref(`${userId}/freeSlots`).orderByKey();
+
+  query.once("value").then(function (snapshot) {
+    snapshot.forEach(function (childSnapshot) {
+      var key = childSnapshot.key;
+      var childData = childSnapshot.val();
+      // console.log("----------");
+      // console.log(
+      //   `Key: ${key} start: ${childData.startDate} end: ${childData.endDate}`
+      // );
+      if (
+        oldStartDateForUpdate.toString() === childData.startDate.toString() &&
+        oldendDateForUpdate.toString() === childData.endDate.toString()
+      ) {
+        console.log("Reached");
+        console.log(data.description);
+        firebase.database().ref(`${userId}/freeSlots`).child(key).update(data);
+        return true; // break out of loop
+      }
+    });
+  });
 };
 
 const retrieve = () => {
-  const freeSlotRef = firebase.database().ref("db/user1232");
-  freeSlotRef.on("value", (snapshot) => {
+  var userRef = firebase.database().ref(userId);
+  userRef.on("value", (snapshot) => {
+    console.log(`Snapshot: ${snapshot.val()}`);
     console.log(snapshot.val());
   });
 };
@@ -68,7 +148,7 @@ class App extends React.Component {
       <div id="scheduler">
         <Scheduler
           timeZone="America/Los_Angeles"
-          dataSource={data}
+          dataSource={dataObject}
           views={views}
           showAllDayPanel={false}
           defaultCurrentView="week"
@@ -77,9 +157,11 @@ class App extends React.Component {
           startDayHour={8}
           endDayHour={24}
           crossScrollingEnabled={true}
-          onAppointmentAdded={onAdded}
+          onAppointmentAdded={addNewSlot}
           onAppointmentDeleted={onDelete}
-          onAppointmentUpdated={retrieve}
+          onAppointmentUpdating={update}
+          // onAppointmentUpdated={updateTest}
+          // onCurrentDateChange={firstTimeUser}
         >
           <Resource
             fieldExpr="priorityId"
